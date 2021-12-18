bfbb97b9b6c8a9250e121f06bcc9be1ae48c339a
macklinhrw <macklingaming@gmail.com>
2021-12-17 18:52:19 -0800

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,19 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body>
-          <ColorModeScript initialColorMode={'dark'} />
+          <ColorModeScript initialColorMode={"dark"} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
